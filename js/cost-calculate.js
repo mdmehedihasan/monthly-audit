204385b3d96bb20cb728c109c1f631db5bca7e5a
mehedi hasan <mehedicsebu@gmail.com>
2022-02-17 17:14:38 +0600

    comment finally updated

--- js/cost-calculate.js
+++ js/cost-calculate.js
@@ -11,8 +11,9 @@ function total_balance(total_monthly_expense) {
     return newBalance;
 }
 
-
+//calculate button 
 document.getElementById('calculate_button').addEventListener('click', function () {
+
     const food_monthly_expense = monthly_cost('food');
     if (food_monthly_expense < 0) {
         return alert('Please put a positive value in Food Field ');
@@ -25,10 +26,13 @@ document.getElementById('calculate_button').addEventListener('click', function (
     if (clothes_monthly_expense < 0) {
         return alert('Please put a positive value in Clothes Field');
     }
+
+    //total expense calculation
     const total_monthly_expense = food_monthly_expense + rent_monthly_expense + clothes_monthly_expense;
 
-    // total balance function call
+    //function call for total balance
     const all_total_balance = total_balance(total_monthly_expense);
+
     //negative income error handle
     const income_total = parseFloat(document.getElementById('income').value);
     if (income_total == 0) {
@@ -38,6 +42,7 @@ document.getElementById('calculate_button').addEventListener('click', function (
         return alert('Salary is less than 0');
     }
 
+    //total balance is less than the cost error handle
     if (total_monthly_expense > all_total_balance) {
         document.getElementById('notification_message').style.display = 'block';
         return alert('Salary is less than cost');
@@ -47,18 +52,20 @@ document.getElementById('calculate_button').addEventListener('click', function (
     let total_cost = document.getElementById('total_expense');
     let total_cost_price = parseFloat(total_cost.innerText);
 
+    //where to show cost
+    total_cost.innerText = total_monthly_expense;
+
     let balance_total = document.getElementById('balance');
     let balance_price = parseFloat(balance_total.innerText);
 
-
     //where to show balance
-    total_cost.innerText = total_monthly_expense;
     balance_total.innerText = all_total_balance;
 })
-//percentage
+//saving percentage button
 document.getElementById('save_button').addEventListener('click', function () {
     let balance_total = document.getElementById('balance');
     let balance_price = parseFloat(balance_total.innerText);
+
     //saving ammount
     const percentage_number = parseFloat(document.getElementById('save_input').value);
     const percentage = (balance_price * percentage_number) / 100;
@@ -70,7 +77,6 @@ document.getElementById('save_button').addEventListener('click', function () {
     if (percentage > balance_price) {
         return alert('Saving is greatert than remaining balance');
     }
-
 
     //where to show saving amount
     let saving_amount_balance = document.getElementById('saving_amount');
